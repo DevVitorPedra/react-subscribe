72d92b882cea274339775b4c7bd3ae00f7ec6c03
DevVitorPedra <75690532+DevVitorPedra@users.noreply.github.com>
2021-12-21 14:32:19 -0300

    heroku url

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -2,7 +2,7 @@ export async function subscribe(name, email, city){
    
     const init = {method:"POST"}
          try {
-            const url = "http://localhost:5000/subs?name=" + name + "&email=" + email + "&city=" + city
+            const url = "https://topbarbersubscribers.herokuapp.com/subs?name=" + name + "&email=" + email + "&city=" + city
             const fetchSub= await fetch(url,init)
             const response = await fetchSub.json()
             console.log(response)
